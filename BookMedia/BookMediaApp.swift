d9f4693c41a443ffe5864d7ee8f4f2280d833322
Dyana Varghese <dyna1.arackal@gmail.com>
2022-10-19 17:57:21 +0100

    Initial Commit

---
+++ BookMedia/BookMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMediaApp.swift
+//  BookMedia
+//
+//  Created by Dyana Varghese on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BookMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
